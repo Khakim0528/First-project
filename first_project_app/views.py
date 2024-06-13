9814ecd544215e1ebe4a4698973805eff747155b
Khakim0528 <khusanovkhakimjon1999@gmail.com>
2024-06-13 15:47:53 +0500

    comment

--- first_project_app/views.py
+++ first_project_app/views.py
@@ -1,5 +1,6 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,25 +14,28 @@ def home(request):
     return HttpResponse(f"<mark> Hello</mark>, My home <hr> "
                         f"Request method: <b style='color:green;'>{request.method}</b>")
 
+
 def home2(request):
     import random
-    a=random.randint(1, 100)
-    b=random.randint(100,1000)
-    c=a+b
+    a = random.randint(1, 100)
+    b = random.randint(100, 1000)
+    c = a + b
     return HttpResponse(f"Random: {a} + {b} = {c} ")
 
 
 def calc(request):
-    a=10
-    b=3
-    c=a+b
-    return  HttpResponse(f" {a} + {b} = {c} ")
+    a = 10
+    b = 3
+    c = a + b
+    return HttpResponse(f" {a} + {b} = {c} ")
+
 
 def calc_int(request, number):
     a = 10
     b = 3
     c = a + b + number
     return HttpResponse(f" {a} + {b} + {number} = {c} ")
+
 
 def get_data(request, text):
     context = {
@@ -42,12 +46,18 @@ def get_data(request, text):
 
 
 class WomenAPIView(APIView):
-    def get(self, request):
-        return Response({
-            'title': 'Anna Ana',
-            'cat_id': '3',
-                         })
 
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id']
+        )
+        return Response({'post', model_to_dict(post_new)})
 
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
