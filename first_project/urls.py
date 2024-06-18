12f2851dcbea08d4f34e8fba856cc6dbf59502fd
Khakim0528 <khusanovkhakimjon1999@gmail.com>
2024-06-18 18:08:30 +0500

    Comment

--- first_project/urls.py
+++ first_project/urls.py
@@ -18,12 +18,34 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
-from first_project_app.views import WomenAPIView
+from first_project_app.views import *
 from first_project_app import views
+from rest_framework import routers
+
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'List'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'})
+    ]
+
+
+router = MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('first_project_app.urls')),
     path('live/', include('live.urls')),
-    path('api/v1/womenlist', WomenAPIView.as_view())
+    path('api/v1/', include(router.urls))
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put':'update'})),
 ]
